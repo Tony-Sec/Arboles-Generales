4277f0d894fc2087347bd337093e5e25de88f1b0
Antonio <37192501+Zayk01@users.noreply.github.com>
2018-04-23 03:04:00 +0200

    Update main.cpp

--- Ejercicios_basicos_Agen/main.cpp
+++ Ejercicios_basicos_Agen/main.cpp
@@ -9,10 +9,9 @@ T_elem fin='#';
 
 int main() {
 
-    Agen<T_elem> A;
+    Agen<T_elem> A,B;
 
-    /***********************************************Leer Arbol General en agen.dat***************************************/
-    cout <<"agen.dat --> Extraer Agen"<<endl;
+    /*********************************************Leer Arbol General en agen.dat***************************************/
     ifstream f_input("..//..//I-O_Agen//agen.dat");
     rellenarAgen(f_input,A);
     f_input.close();
@@ -23,6 +22,22 @@ int main() {
     T_elem elem =A.elemento(A.hijoIzqdo(A.hermDrcho(A.hijoIzqdo(A.raiz()))));
     cout<<"El elemento "<<elem<<" es hijo?"<<is_sheet(A,A.hijoIzqdo(A.hermDrcho(A.hijoIzqdo(A.raiz())))) <<endl;
     cout<<"El grado del Arbol General es: "<<grade(A)<<endl;
+    cout<<"La profundidad del nodo "<<elem<<" es :"<<deep(A,A.hijoIzqdo(A.hermDrcho(A.hijoIzqdo(A.raiz())))) <<endl;
+    cout<<"El Desequilibrio del Arbol General es:  "<<disequilibrium(A) <<endl;
+
+
+    /********************************************Leer Arbol General en agen2.dat***************************************/
+    ifstream f_input2("..//..//I-O_Agen//agen2.dat");
+    rellenarAgen(f_input2,B);
+    f_input2.close();
+
+    cout <<"\n\nCaracteristicas del Arbol Binario B************************************:\n"<<endl;
+    imprimirAgen(B);
+    cout<<"\n\n";
+    cout<<"El Desequilibrio del Arbol General es:  "<<disequilibrium(B) <<endl;
+
+
+
 
     return 0;
 }
